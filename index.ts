4e083002ace9ab8fc0efdfced5944014a0e94cc5
Bradley Walters <oss@walters.app>
2020-07-28 19:32:56 -0600

    Switch to csstree for style validation and transformation
    Implement fixing up invalid regexp() special character escapes

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { URL } from "url"
 import fetch from "node-fetch"
 import { promisify } from "util"
 import { parse, Metadata, Variable } from "usercss-meta"
-import { lint } from "stylelint"
+import * as csstree from "css-tree"
 import * as yargs from "yargs"
 
 const replaceAll = (str: string, find: string, replace: string) =>
@@ -70,30 +70,133 @@ const assignVariables = (
 	}
 }
 
-// TODO: Make this smarter?
-const adjustPriority = (styleText: string): string =>
-	styleText.replace(/(: ?[^\!]*?);\n/g, "$1 !important;\n")
+/**
+ * Fixes special character escaping for regexp() arguments in CSS.
+ *
+ * For example, some userstyles write "\w" when "\\w" is required.
+ * One escape is necessary for the regexp, another for the CSS string.
+ *
+ * See https://developer.mozilla.org/en-US/docs/Web/CSS/@document#Syntax
+ *
+ * @param regexp regexp argument to fix
+ */
+const escapeCSSRegExp = (regexp: string): string => {
+	if (regexp.includes(String.raw`\\`)) {
+		// Assume already properly escaped if the regexp contains \\
+		return regexp
+	}
 
-const validateCSS = async (styleText: string): Promise<void> => {
-	const result = await lint({
-		code: styleText,
-		syntax: "css" as any,
-		config: {
-			extends: "stylelint-config-recommended",
-			rules: {
-				"no-duplicate-selectors": null,
-				"no-descending-specificity": null,
-				"keyframe-declaration-no-important": null,
-				"declaration-block-no-shorthand-property-overrides": null,
-				"declaration-block-no-duplicate-properties": null,
-				"font-family-no-duplicate-names": null,
-				"selector-type-no-unknown": null,
-			},
+	// Replace single-backslash escapes with double-backslashes.
+	return regexp.replace(/([^\\]\\)/g, "$1\\")
+}
+
+/**
+ * Validates CSS and applies transformations necessary for userContent.css.
+ *
+ * @param css css to validate and transform
+ */
+const validateAndTransformCSS = (
+	css: string,
+): { errors: string[]; warnings: string[]; transformed: string } => {
+	const errors = []
+	const ast = csstree.parse(css, {
+		positions: true,
+		onParseError: (error) => errors.push(error),
+	})
+
+	const stringifyError = (e) =>
+		`${e.line}:${e.column} (${e.property}): ${e.message}\n${e.error}`
+
+	// https://github.com/csstree/validator/blob/73c7a745a860bb9fb88cb463f0/LICENSE
+	csstree.walk(ast, {
+		visit: "Declaration",
+		enter: function (node) {
+			const match = (csstree as any).lexer.matchDeclaration(node)
+			const error = match.error
+			if (!error) return
+
+			let message = error.rawMessage || error.message || error
+
+			// ignore errors except those which make sense
+			if (
+				error.name !== "SyntaxMatchError" &&
+				error.name !== "SyntaxReferenceError"
+			) {
+				return
+			}
+
+			if (message === "Mismatch") {
+				message = "Invalid value for `" + node.property + "`"
+			}
+
+			const errorReport = {
+				name: error.name,
+				node: node,
+				loc: error.loc || node.loc,
+				line: error.line || (node.loc && node.loc.start && node.loc.start.line),
+				column:
+					error.column || (node.loc && node.loc.start && node.loc.start.column),
+				property: node.property,
+				message: message,
+				error: error,
+			}
+
+			errors.push(stringifyError(errorReport))
 		},
 	})
 
-	if (result.errored)
-		throw new Error(`Interpolated CSS failed to validate:\n${result.output}`)
+	if (errors.length) {
+		return { errors, warnings: [], transformed: css }
+	}
+
+	// Mark all declarations as !important.
+	// This is necessary because userstyles are written for
+	// injection at a higher precedence than page styles.
+	csstree.walk(ast, {
+		visit: "Declaration",
+		enter: (node) => {
+			node.important = true
+		},
+	})
+
+	const warnings = []
+
+	// Fix escaping inside regexp() blocks.
+	// See documentation for `escapeCSSRegExp`.
+	// TODO: Why do these buggy styles work in Stylus?
+	csstree.walk(ast, {
+		visit: "AtrulePrelude",
+		enter: function (node) {
+			if (
+				this.atrule.name !== "-moz-document" &&
+				this.atrule.name !== "document"
+			)
+				return
+
+			node.children.forEach((preludeFunction) => {
+				if (
+					preludeFunction.type !== "Function" ||
+					preludeFunction.name !== "regexp"
+				)
+					return
+
+				const regexp = preludeFunction.children.first()
+				const loc = `${regexp.loc.start.line}:${regexp.loc.start.column}`
+				if (regexp.type !== "String") {
+					errors.push(`${loc}: regexp() argument must be a String`)
+					return
+				}
+
+				const escaped = escapeCSSRegExp(regexp.value)
+				if (regexp.value !== escaped) {
+					warnings.push(`${loc}: Fixed escaping in regexp(${regexp.value})`)
+				}
+				regexp.value = escaped
+			})
+		},
+	})
+
+	return { errors, warnings, transformed: csstree.generate(ast) }
 }
 
 const fetchStyle = async (urlString: string): Promise<string> => {
@@ -107,6 +210,7 @@ const fetchStyle = async (urlString: string): Promise<string> => {
 }
 
 const main = async () => {
+	// TODO: Allow user to specify a Firefox profile and take care of it from there?
 	const argv = yargs
 		.usage(
 			"Usage: $0 --user-styles <path> --output-styles-dir <path> --output-imports-file <path>",
@@ -145,26 +249,44 @@ const main = async () => {
 	const stylePaths = await Promise.all(
 		styleUrls.map(async (styleUrl) => {
 			// TODO: Keep track of currently installed version?
+			// Fetch the raw userstyle from its URL.
 			const rawStyle = await fetchStyle(styleUrl)
+			const filename = path.basename(new URL(styleUrl).pathname)
 
+			// Parse the userstyle metadata block from the style file.
 			const { metadata, errors } = parse(rawStyle, { mandatoryKeys: [] })
 			if (errors.length) {
 				throw new Error(errors.join("\n"))
 			}
 
-			const style = adjustPriority(assignVariables(rawStyle, metadata))
+			// Perform preprocessing on the userstyle to transform it into valid CSS.
+			const css = assignVariables(rawStyle, metadata)
 
-			const filename = path.basename(new URL(styleUrl).pathname)
+			// Validate the generated CSS file and apply fixes for userContent.css.
+			const {
+				transformed,
+				warnings: validationWarnings,
+				errors: validationErrors,
+			} = validateAndTransformCSS(css)
+
+			// Throw an error if any parsing / validation errors occurred.
+			if (validationErrors.length) {
+				throw new Error(
+					`CSS Validation Errors: ${filename}\n${validationErrors.join("\n")}`,
+				)
+			}
+
+			// Inform the user of any validation warnings that occurred.
+			if (validationWarnings.length) {
+				const msg = validationWarnings.join("\n")
+				console.log(`CSS Validation Warnings: ${filename}\n${msg}\n`)
+			}
+
+			// Write the generated CSS file to `output-styles-dir`.
 			const stylePath = path.join(argv["output-styles-dir"], filename)
 
 			await promisify(fs.mkdir)(argv["output-styles-dir"], { recursive: true })
-			await promisify(fs.writeFile)(stylePath, style)
-
-			try {
-				await validateCSS(style)
-			} catch (error) {
-				if (argv["ignore-lint"] !== true) throw error
-			}
+			await promisify(fs.writeFile)(stylePath, transformed)
 
 			return stylePath
 		}),
